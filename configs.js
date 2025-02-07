6b01c1f2e633d52f9e9ced572e2ad454a43001ae
Rick Hsu <rickhsu@systex.com>
2025-02-07 17:59:40 +0800

    custom parameter

--- configs.js
+++ configs.js
@@ -1,18 +1,18 @@
 YL.static = {
   /** “关于”信息 */
-  softwareName: 'YLUI DEMO', //网站名。请在此处填写您自己的网站名，如王小明的博客。
+  softwareName: 'Rick Win10 UI', //网站名。请在此处填写您自己的网站名，如王小明的博客。
   version: "1.0.0", // 网站版本号
-  iconBtnStart: 'html5', //主图标
-  author: 'null',//作者
-  contactInformation: 'null',//联系方式
-  officialWebsite: '//null',//软件官网
-  welcome: '本网站UI由 YLUI 强力驱动\n更多信息：//ylui.yuri2.cn',//加载完毕控制台提示信息
-  copyrightDetail: 'null',//版权详细信息
+  iconBtnStart: 'rocket', //主图标
+  author: 'Rick Hsu',//作者
+  contactInformation: 'as123789043@gmail.com',//联系方式
+  officialWebsite: 'as183789043.github.io',//软件官网
+  welcome: '本網站UI由YLUI提供',//加载完毕控制台提示信息
+  copyrightDetail: 'Rick',//版权详细信息
   otherStatements: '',//其他信息（可留空）
 
   /**————————————————————————————————————————————————————————————————————————————————————————————*/
   /** YLUI基础设置 */
-  lang: 'zh-cn', //语言
+  lang: 'zh-tw', //语言
   localStorageName: "ylui-storage", //ls存储名
   lockedApps: ['yl-system', 'yl-color-picker', 'ylui-fa', 'yl-browser'], // 锁定的应用（不允许被脚本修改）
   trustedApps: [], // 受信任的应用（可以使用敏感API）
@@ -25,7 +25,7 @@ YL.static = {
 
   /**————————————————————————————————————————————————————————————————————————————————————————————*/
   /** YLUI注册信息 */
-  authorization: '社区版',//授权类型
-  serialNumber: null,//序列号
+  authorization: '個人版',//授权类型
+  serialNumber: '1234567890',//序列号
 
 };
